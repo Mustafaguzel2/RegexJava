445e1b4aa1e0f9b42a382b923f4ee90cacd71001
Mustafa Güzel <mustafaguzel879@gmail.com>
2024-08-25 15:48:47 +0300

    added some comment

--- .vscode/main.java
+++ .vscode/main.java
@@ -6,7 +6,13 @@ public class main {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("agent \\d{3,5}");
         Matcher matcher = pattern.matcher("agent 0075");
-        /*\\d{}bu bize sınırsız digit girme sağlar ve match eder */
+        /*\\d{}bu bize sınırsız digit girme sağlar ve match eder 
+         * ^ eğer bu işaret başında olursa ve $ bu işarette sonunda olursa
+         * sadece bizim yazdığımız regex şekli true döner ancak başında
+         * veya sonunda bunlar olmazsa içinde bizim regex kodumuzun olması
+         * true dönmesi için yeterli olur.
+        */
+
         boolean found = matcher.find();
 
         System.out.println("found: " + found);
