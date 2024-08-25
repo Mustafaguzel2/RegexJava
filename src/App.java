db729bb51499cc0643022acf8ff466e669acf55c
Mustafa Güzel <mustafaguzel879@gmail.com>
2024-08-25 16:05:04 +0300

    another character represtation showed and added comment

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        
+        Pattern pattern = Pattern.compile("^\\w* \\d{3,5}$");
+        Matcher matcher = pattern.matcher("agent 0214");
+        /* 1. \\d{}bu bize sınırsız digit girme sağlar ve match eder 
+         * 2. ^ eğer bu işaret başında olursa ve $ bu işarette sonunda olursa
+         * sadece bizim yazdığımız regex şekli true döner ancak başında
+         * veya sonunda bunlar olmazsa içinde bizim regex kodumuzun olması
+         * true dönmesi için yeterli olur.
+         * 3. [0-9] ibaresi hangi rakamların kabul göreceğini söyler.
+         * [0-9]{3,5} gibi kullanılması gerekir.
+         * 4. '.' her türlü karakteri temsil eder.
+         * 5. [aA] ise hangi karakterlerin kabul göreceğini gösterir.
+         * 6. (a|A|aa) ifadesi birden fazla karakterin aynı anda
+         * kabul görmesini sağlar.
+         * [abc] a veya b veya c kabul görür.
+         * [a-z] arasındaki harfler kabul görür. (fakat non-Capslock)
+         * 7. \\w* herhangi bir karakterin kullanımını söyler[a-zA-Z_0-9].
+         * agent007 ag007 gibi ifadeler true
+         * \\w+ bir veya daha fazla eşleşme varsa true döner.
+         * agent007 true
+         * \\w? bir veya daha az eşleşme varsa true döner.
+         * 007 true veya t007 true
+         * 
+        */
+
+        boolean found = matcher.find();
+
+        System.out.println("found: " + found);
     }
 }
